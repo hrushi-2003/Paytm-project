17caee3754f77d3a5aa325f848cafec308b656f5
Hrushikesh Reddy <hrushikesh200802@gmail.com>
2024-10-24 16:31:40 +0530

    route for search filter is added

--- backend/route/user.route.js
+++ backend/route/user.route.js
@@ -1,6 +1,14 @@
 import express from "express";
-import { createAccount, login } from "../controller.js/user.controller.js";
+import {
+  createAccount,
+  getUsers,
+  login,
+  updateProfile,
+} from "../controller.js/user.controller.js";
+import isAuthenticated from "../middleware/isAuthenticated.js";
 const router = express.Router();
 router.route("/createAccount").post(createAccount);
 router.route("/login").get(login);
+router.route("/updateProfile").put(isAuthenticated, updateProfile);
+router.route("/getUsers").get(isAuthenticated, getUsers);
 export default router;
