2b5aa92d41ce33a7a9315c33578989b3f22c4d4d
Hrushikesh Reddy <hrushikesh200802@gmail.com>
2024-10-24 15:04:02 +0530

    updated

--- backend/route/user.route.js
+++ backend/route/user.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { createAccount } from "../controller.js/user.controller.js";
+import { createAccount, login } from "../controller.js/user.controller.js";
 const router = express.Router();
 router.route("/createAccount").post(createAccount);
+router.route("/login").get(login);
 export default router;
