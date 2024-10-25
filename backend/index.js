fee67562dc539535f82203bfc403c5549c57e571
Hrushikesh Reddy <hrushikesh200802@gmail.com>
2024-10-25 19:28:27 +0530

    added account route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./route/user.route.js";
+import accountRoute from "./route/account.route.js";
 import cors from "cors";
 dotenv.config();
 const connectDb = async () => {
@@ -16,6 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/account", accountRoute);
 const Port = process.env.PORT || 3000;
 app.listen(Port, () => {
   console.log(`app listening to the port ${Port}`);
