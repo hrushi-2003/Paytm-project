ba3c5281c22ac5cdaf46514d07dde4f11259380d
Hrushikesh Reddy <hrushikesh200802@gmail.com>
2024-10-29 09:48:45 +0530

    get user list in dashboard code written

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router";
 import ButtonBox from "./ButtonBox";
 const token = localStorage.getItem("token");
 const User = () => {
@@ -40,6 +41,7 @@ function Users({ user }) {
   const symbolOfUser = user.firstname[0].toUpperCase();
   const firstname = user.firstname.toUpperCase();
   const lastname = user.lastname.toUpperCase();
+  const navigate = useNavigate();
   return (
     <div className="flex justify-between m-4">
       <div className="flex">
@@ -47,12 +49,14 @@ function Users({ user }) {
           <div className="flex flex-col justify-center ">{symbolOfUser}</div>
         </div>
         <div className="flex flex-col justify-center ml-4">
-          {firstname+" "}
+          {firstname + " "}
           {lastname}
         </div>
       </div>
       <div>
-        <ButtonBox label={"Send Money"} />
+        <ButtonBox onClick={()=>{
+          navigate('/send?id='+user._id+"&name="+firstname)
+        }} label={"Send Money"} />
       </div>
     </div>
   );
