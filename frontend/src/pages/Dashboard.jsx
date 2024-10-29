7d43664451601339da18c5a581003485ab1fab89
Hrushikesh Reddy <hrushikesh200802@gmail.com>
2024-10-29 09:47:47 +0530

    dashboard updated

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import AppBar from "../components/AppBar";
+import Balance from "../components/Balance";
+import User from "../components/User";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <AppBar />
+      <div className="mt-8 ml-2 p-4">
+        <Balance/>
+        <User/>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
