34d4caa7a24279972ab6f7a31f71c7c1c0af08a1
Hrushikesh Reddy <hrushikesh200802@gmail.com>
2024-12-10 23:17:10 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -4,23 +4,24 @@ import dotenv from "dotenv";
 import userRoute from "./route/user.route.js";
 import accountRoute from "./route/account.route.js";
 import cors from "cors";
+
 dotenv.config();
+
 const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDb connected successfully");
   } catch (error) {
-    console.log("error in mongodb connection");
+    console.log("Error in MongoDB connection:", error.message);
   }
 };
+
+connectDb(); // Connect to the database when the function is invoked
+
 export const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/account", accountRoute);
-const Port = process.env.PORT || 3000;
-app.listen(Port, () => {
-  console.log(`app listening to the port ${Port}`);
-  connectDb();
-});
-export default app;
+
+export default app; // No app.listen here
